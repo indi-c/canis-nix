77f37101bb0bb6a03c6c16201afc2673e9de7a68
indi-c <indigoconnelly@pm.me>
2025-05-31 18:38:40 +0100

    initial flake configuration

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = ''
+  a basic flake based configuration for NixOS
+  '';
+
+  inputs = {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, home-manager, nixpkgs, ... }@inputs:
+    let
+      inherit (self) outputs;
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages =
+        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      overlays = import ./overlays { inherit inputs; };
+      nixosConfigurations = {
+        NL_I = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/NL_I];
+        };
+      };
+      homeConfigurations = {
+        "indicanis@NL_I" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [ ./home/indicanis/NL-I.nix ];
+        };
+      };
+    };
+}
